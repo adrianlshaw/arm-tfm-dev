07771ae574eb3201487e9681ce8307b176bf52a4
Adrian L. Shaw <adrianlshaw@gmail.com>
2020-09-25 18:43:37 +0100

    Bump TF-M version and change build script for new cmake system

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,8 +1,7 @@
-set -e
+set -ex
 
 cd trusted-firmware-m/
-rm -rf build
-mkdir build
-cd build/
-cmake ../ -G"Unix Makefiles" -DPROJ_CONFIG=`readlink -f ../configs/ConfigRegressionIPC.cmake` -DTARGET_PLATFORM=AN521 -DCMAKE_BUILD_TYPE=Debug -DCOMPILER=GNUARM -DBL2=True && make
-timeout --foreground 10 qemu-system-arm -M mps2-an521 -kernel bl2/ext/mcuboot/mcuboot.axf  -device loader,file=tfm_sign.bin,addr=0x10080000 -serial stdio -display none || true
+cmake -S . -B cmake_build -DTFM_PLATFORM=mps2/an521 -DCMAKE_TOOLCHAIN_FILE=toolchain_GNUARM.cmake -DCMAKE_BUILD_TYPE=Debug -DTFM_PROFILE=profile_small -DTEST_PSA_API=CRYPTO
+cmake --build cmake_build -- install
+#cmake ../ -G"Unix Makefiles" -DPROJ_CONFIG=`readlink -f ../configs/ConfigRegressionIPC.cmake` -DTARGET_PLATFORM=AN521 -DCMAKE_BUILD_TYPE=Debug -DCOMPILER=GNUARM -DBL2=True && make
+timeout --foreground 10 qemu-system-arm -M mps2-an521 -kernel cmake_build/bin/bl2.axf  -device loader,file=cmake_build/bin/tfm_s_ns_signed.bin,addr=0x10080000 -serial stdio -display none || true
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,7 +5,7 @@ apt install -y build-essential python3-dev python3-pip gcc g++ make \
 	git build-essential wget python3-pip python3 git-lfs python libglib2.0-dev \
 	libfdt-dev libpixman-1-dev zlib1g-dev cmake
 
-apt install gcc-arm-none-eabi -y
+apt install qemu-system-arm gcc-arm-none-eabi -y
 
 #wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
 #tar xvjf gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
@@ -17,8 +17,4 @@ apt install gcc-arm-none-eabi -y
 #cp -r $PWD/cmake-3.11.1-Linux-x86_64/bin/* /usr/local/bin/
 
 
-pip3 install cryptography pycrypto pyasn1 cbor
-
-wget https://download.qemu.org/qemu-4.1.0.tar.xz
-tar xf qemu-4.1.0.tar.xz
-cd qemu-4.1.0 && ./configure --target-list=arm-softmmu && make -j4 && make install
+pip3 install imgtool cryptography pycrypto pyasn1 cbor jinja2 pyyaml click intelhex
