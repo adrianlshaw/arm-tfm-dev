9a42ea41db68c1a7b4a64c73088dc55e758aeedd
Adrian L. Shaw <adrianlshaw@gmail.com>
2020-12-18 00:29:27 +0000

    Fix

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,4 @@
-set -ex
+set -x
 
 if [ -z $(git config --list | grep email) ]; then
 	git config --global user.email "you@example.com"
@@ -8,8 +8,10 @@ fi
 cd trusted-firmware-m/
 cmake -S . -B cmake_build -DTFM_PLATFORM=mps2/an521 -DTFM_TOOLCHAIN_FILE=toolchain_GNUARM.cmake -DCMAKE_BUILD_TYPE=Debug -DTFM_PROFILE=profile_small -DTEST_PSA_API=CRYPTO
 HERE=$PWD
-cd trusted-firmware-m/cmake_build/lib/ext/mbedcrypto-src && git apply ../../../../lib/ext/mbedcrypto/*.patch
+echo "applying patch"
+cd cmake_build/lib/ext/mbedcrypto-src && git apply ../../../../lib/ext/mbedcrypto/*.patch
 cd $HERE
+echo "back to $HERE"
 cmake -S . -B cmake_build -DTFM_PLATFORM=mps2/an521 -DTFM_TOOLCHAIN_FILE=toolchain_GNUARM.cmake -DCMAKE_BUILD_TYPE=Debug -DTFM_PROFILE=profile_small -DTEST_PSA_API=CRYPTO
 cmake --build cmake_build -- install
 timeout --foreground 10 qemu-system-arm -M mps2-an521 -kernel cmake_build/bin/bl2.axf  -device loader,file=cmake_build/bin/tfm_s_ns_signed.bin,addr=0x10080000 -serial stdio -display none || true
