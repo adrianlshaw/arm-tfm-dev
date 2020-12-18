2febb626a111b74a1f7bc15f5918e09378e5f5c3
Adrian L. Shaw <adrianlshaw@gmail.com>
2020-12-18 00:11:53 +0000

    Minor fix

--- build-and-run.sh
+++ build-and-run.sh
@@ -7,7 +7,7 @@ fi
 
 cd trusted-firmware-m/
 cmake -S . -B cmake_build -DTFM_PLATFORM=mps2/an521 -DTFM_TOOLCHAIN_FILE=toolchain_GNUARM.cmake -DCMAKE_BUILD_TYPE=Debug -DTFM_PROFILE=profile_small -DTEST_PSA_API=CRYPTO
-cd trusted-firmware-m/cmake_build/lib/ext/mbedcrypto-src && git apply ../../../../lib/ext/mbedcrypto/*.patch && ../../../../
+cd trusted-firmware-m/cmake_build/lib/ext/mbedcrypto-src && git apply ../../../../lib/ext/mbedcrypto/*.patch && cd ../../../../../
 cmake -S . -B cmake_build -DTFM_PLATFORM=mps2/an521 -DTFM_TOOLCHAIN_FILE=toolchain_GNUARM.cmake -DCMAKE_BUILD_TYPE=Debug -DTFM_PROFILE=profile_small -DTEST_PSA_API=CRYPTO
 cmake --build cmake_build -- install
 timeout --foreground 10 qemu-system-arm -M mps2-an521 -kernel cmake_build/bin/bl2.axf  -device loader,file=cmake_build/bin/tfm_s_ns_signed.bin,addr=0x10080000 -serial stdio -display none || true
